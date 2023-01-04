1dccb72b6b67f2725fb4d93ec4589f8e4e4e78a7
NastassiaStankevich <nastystankevich94@gmail.com>
2023-01-05 01:30:16 +0300

    First java steps

---
+++ FirstSteps/src/com/epam/learn/main/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.epam.learn.main;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.print(42);
+        System.out.print(42);
+        System.out.print("\n\tJava" + 16);
+    }
+}
