14d7c5d29a07e0041420552ac1c6a13486266330
NastassiaStankevich <nastystankevich94@gmail.com>
2023-01-05 01:34:40 +0300

    First java steps(input int)

--- FirstSteps/src/com/epam/learn/main/InputStringProgram.java
+++ FirstSteps/src/com/epam/learn/main/InputStringProgram.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class InputStringProgram {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String input = scan.next();
-        System.out.println("Hello " + input);
+
+        int number = scan.nextInt();
+
+        System.out.println(number);
     }
 }
